8194e91dad662d11ffc5f8eb699ccc345d536ad1
Matias Ferreyra <103839740+matyferreyra@users.noreply.github.com>
2023-05-03 23:00:07 -0300

    Update funciones.js

--- assets/JavaScript/funciones.js
+++ assets/JavaScript/funciones.js
@@ -15,9 +15,7 @@ function search() {
     }
 }
 
-
-
-function searching(productos, searchKey) {
+function searching(productos, searchKey) { //Está función emplea para la mejor busqueda posible el método "filter", y así devolver todos los objetos que coincidan con la palabra clave. Me parece un método más efectivo que usar un "FIND" para encontrar solo al primer objeto.
     return productos.filter(producto => {
         const filtrado = producto.nombre + " " + producto.categoria;
         return filtrado.toLowerCase().includes(searchKey.toLowerCase());
@@ -25,6 +23,16 @@ function searching(productos, searchKey) {
 }
 
 
-function add_Product( //FUNCION EN CONSTRUCCION. IGNORAR SU EVALUACIÓN DE MOMENTO
-    let addOn = Productos.push();
-) { }
+function add_Product() {
+    let nombre = prompt("Ingrese el nombre del producto:");
+    let categoria = prompt("Ingrese la categoría del producto:");
+    let precio = prompt("Ingrese el precio del producto:");
+    let nuevoProducto = new Producto(nombre, categoria, precio);
+    Productos.push(nuevoProducto);
+    console.log("El producto ha sido agregado correctamente.");
+}
+
+
+function delete_Product (){}
+
+function replace_Product (){}
